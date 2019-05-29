6eb59dc5dafba357de66b3ce00b12aaa4140433c
zmiter <zmiter.mde@gmail.com>
2019-05-29 17:39:40 +0300

    Refactored config for different environments

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080';
+import Config from '../Config';
+
 export const ACCESS_TOKEN = 'accessToken';
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2redirect';
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/GOOGLE?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const OAUTH2_REDIRECT_URI = Config.API.clientUrl + 'oauth2redirect';
+export const GOOGLE_AUTH_URL = Config.API.serverUrl + 'oauth2/authorize/GOOGLE?redirect_uri=' + OAUTH2_REDIRECT_URI;
